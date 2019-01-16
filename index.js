0e2e7c4f7cf2be8b8570b6de72122ce5aa9d3849
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-01-16 17:29:11 -0200

    Update ClipboardButton and rename it to ClipboardWrapper

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-class ClipboardButton extends React.Component {
+class ClipboardWrapper extends React.Component {
   static propTypes = {
     options: function(props, propName, componentName) {
       const options = props[propName];
-      if (options && typeof options !== 'object' || Array.isArray(options)) {
-        return new Error(`Invalid props '${propName}' supplied to '${componentName}'. ` +
-        `'${propName}' is not an object.`);
+      if ((options && typeof options !== "object") || Array.isArray(options)) {
+        return new Error(
+          `Invalid props '${propName}' supplied to '${componentName}'. ` +
+            `'${propName}' is not an object.`
+        );
       }
 
-      if (props['option-text'] !== undefined) {
-        const optionText = props['option-text'];
-        if (typeof optionText !== 'function') {
-          return new Error(`Invalid props 'option-text' supplied to '${componentName}'. ` +
-          `'option-text' is not a function.`);
+      if (props["option-text"] !== undefined) {
+        const optionText = props["option-text"];
+        if (typeof optionText !== "function") {
+          return new Error(
+            `Invalid props 'option-text' supplied to '${componentName}'. ` +
+              `'option-text' is not a function.`
+          );
         }
       }
     },
-    type: PropTypes.string,
-    className: PropTypes.string,
-    style: PropTypes.object,
-    component: PropTypes.string,
     children: PropTypes.oneOfType([
       PropTypes.element,
       PropTypes.arrayOf(PropTypes.element),
@@ -29,11 +29,11 @@ class ClipboardButton extends React.Component {
       PropTypes.number,
       PropTypes.object,
     ]),
-  }
+  };
 
   static defaultProps = {
     onClick: function() {},
-  }
+  };
 
   /* Returns a object with all props that fulfill a certain naming pattern
    *
@@ -53,12 +53,12 @@ class ClipboardButton extends React.Component {
    * this.propsWith(on*, true); // returns {Bar: 2}
    * this.propsWith(data-*); // returns {data-foobar: 1, data-baz: 4}
    */
-  propsWith(regexp, remove=false) {
+  propsWith(regexp, remove = false) {
     const object = {};
 
     Object.keys(this.props).forEach(function(key) {
       if (key.search(regexp) !== -1) {
-        const objectKey = remove ? key.replace(regexp, '') : key;
+        const objectKey = remove ? key.replace(regexp, "") : key;
         object[objectKey] = this.props[key];
       }
     }, this);
@@ -74,46 +74,28 @@ class ClipboardButton extends React.Component {
     // Support old API by trying to assign this.props.options first;
     const options = this.props.options || this.propsWith(/^option-/, true);
     const element = React.version.match(/0\.13(.*)/)
-      ? this.refs.element.getDOMNode() : this.element;
-    const Clipboard = require('clipboard');
+      ? this.refs.element.getDOMNode()
+      : this.element;
+    const Clipboard = require("clipboard");
     this.clipboard = new Clipboard(element, options);
 
     const callbacks = this.propsWith(/^on/, true);
     Object.keys(callbacks).forEach(function(callback) {
-      this.clipboard.on(callback.toLowerCase(), this.props['on' + callback]);
+      this.clipboard.on(callback.toLowerCase(), this.props["on" + callback]);
     }, this);
   }
 
   render() {
-    const attributes = {
-      type: this.getType(),
-      className: this.props.className || '',
-      style: this.props.style || {},
-      ref: element => { this.element = element; },
-      onClick: this.props.onClick,
-      ...this.propsWith(/^data-/),
-      ...this.propsWith(/^button-/, true),
-    };
-
-    return React.createElement(
-      this.getComponent(),
-      attributes,
-      this.props.children
+    return (
+      <div
+        ref={element => {
+          this.element = element;
+        }}
+      >
+        {this.children}
+      </div>
     );
-  }
-
-  getType() {
-    if (this.getComponent() === 'button' || this.getComponent() === 'input') {
-      return this.props.type || 'button';
-    }
-    else {
-      return undefined;
-    }
-  }
-
-  getComponent() {
-    return this.props.component || 'button';
   }
 }
 
-export default ClipboardButton;
+export default ClipboardWrapper;
